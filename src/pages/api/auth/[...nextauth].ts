ccaaa22a4283726854d68f285001b0ee495b0932
Colin Palombo <colin.palombo@yahoo.com>
2022-08-17 20:38:58 -0500

    More auth providers

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,18 +4,35 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 
 import NextAuth from "next-auth";
+
 import GitHubProvider from "next-auth/providers/github";
+import DiscordProvider from "next-auth/providers/discord";
+import Auth0Provider from "next-auth/providers/auth0";
 
 const prisma = new PrismaClient();
 
 export const authOptions: NextAuthOptions = {
     adapter: PrismaAdapter(prisma),
     providers: [
+        Auth0Provider({
+            name: "Email",
+            clientId: process.env.AUTH0_ID as string,
+            clientSecret: process.env.AUTH0_SECRET as string,
+            issuer: process.env.AUTH0_ISSUER as string
+        }),
+        DiscordProvider({
+            clientId: process.env.DISCORD_ID as string,
+            clientSecret: process.env.DISCORD_SECRET as string
+        }),
         GitHubProvider({
             clientId: process.env.GITHUB_ID as string,
-            clientSecret: process.env.GITHUB_SECRET as string,
+            clientSecret: process.env.GITHUB_SECRET as string
         })
-    ]
+    ],
+    theme: {
+        logo: "/images/logo.png",
+        colorScheme: "light"
+    }
 };
 
 export default NextAuth(authOptions);
